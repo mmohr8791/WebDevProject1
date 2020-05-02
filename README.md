012f031e2ab65fb4f775d8c5f7d9330ea06a26da
mike.mohr <mmohr.55987@gmail.com>
2020-05-01 20:00:29 -0500

    Modified html,css files.  Added image files.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 # Created By:		Mike Mohr
 # Created On:		04/18/20
 # Technologies Used:	.html, .css, .js, .jpg, .pdf, .doc
+#
+# This project has been linked to the following GitHub repository:
+# https://github.com/mmohr8791/WebDevProject1.git
+# <end of README file>
---
+++ js/loginPage.js
@@ -0,0 +1,25 @@
+/*
+var slideInterval = 3500;
+function getFigures(){
+    return document.getElementById('carousel').getElementsByTagName('figure');
+}
+function moveForward(){
+    var pointer;
+    var figures = getFigures();
+    for (var i=0; i < figures.length; i++){
+        if (figures[i].className == 'visible'){
+            figures[i].className = '';
+            pointer = i;    
+        }
+    }
+    if (++pointer == figures.length) {
+        pointer = 0;
+    }
+    figures[pointer].className = 'visible';
+    setTimeout(moveForward, slideInterval);
+}
+function startplayback() {
+    setTimeout(moveForward, slideInterval);
+}
+startplayback();
+*/
